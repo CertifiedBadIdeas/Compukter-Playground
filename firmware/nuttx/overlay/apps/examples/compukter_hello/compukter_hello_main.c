6fafe494507bfc324d3c14d43bc660cdc0d56f4d
lazyhat <sevapetrov2016p@gmail.com>
2026-08-16 21:24:23 +0300

    feat(nuttx): expose NSH acceptance firmware (#1)

---
+++ firmware/nuttx/overlay/apps/examples/compukter_hello/compukter_hello_main.c
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#include <nuttx/config.h>
+
+#include <stdio.h>
+
+int main(int argc, FAR char *argv[])
+{
+  printf("Hello from NuttX on Compukter-VM\n");
+  return 0;
+}
